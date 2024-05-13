3692bbbe80a51295796211f332f01c34b257a0e6
bliepp <12013202+bliepp@users.noreply.github.com>
2024-05-13 16:09:34 +0200

    Init project, based on gorilla websocket's chat example

---
+++ hub.go
@@ -0,0 +1,74 @@
+package main
+
+import (
+	"github.com/gorilla/websocket"
+)
+
+type Broadcast struct {
+	topic      string
+	clients    map[*Client]bool
+	messages   chan []byte
+	register   chan *Client
+	unregister chan *Client
+}
+
+var broadcasts map[string]*Broadcast = make(map[string]*Broadcast)
+
+func NewBroadcast(topic string) *Broadcast {
+	// reuse existing broadcasts on the same topic
+	b, ok := broadcasts[topic]
+	if ok {
+		return b
+	}
+
+	// if there is no matching boradcast for given topic,
+	// create a new one and start the goroutine
+	b = &Broadcast{
+		topic:      topic,
+		clients:    make(map[*Client]bool),
+		messages:   make(chan []byte),
+		register:   make(chan *Client),
+		unregister: make(chan *Client),
+	}
+	broadcasts[topic] = b
+	go b.Run()
+
+	return b
+}
+
+// create client that is attached to that broadcast
+func (b *Broadcast) NewClient(c *websocket.Conn) *Client {
+	return NewClient(b, c)
+}
+
+func (b *Broadcast) Run() {
+	done := make(chan bool)
+
+	for {
+		select {
+		case client := <-b.register:
+			b.clients[client] = true
+		case client := <-b.unregister:
+			if _, ok := b.clients[client]; ok {
+				delete(b.clients, client)
+				close(client.send)
+			}
+
+			if len(b.clients) == 0 {
+				delete(broadcasts, b.topic)
+				done <- true // break the goroutine
+			}
+		case msg := <-b.messages:
+			for client := range b.clients {
+				select {
+				case client.send <- msg:
+				default:
+					delete(b.clients, client)
+					close(client.send)
+				}
+			}
+		case <-done:
+			return
+		}
+	}
+}
---
+++ main.go
@@ -0,0 +1,110 @@
+package main
+
+import (
+	"html/template"
+	"log"
+	"net/http"
+	"strings"
+
+	"github.com/gorilla/websocket"
+)
+
+var upgrader = websocket.Upgrader{}
+
+func main() {
+	http.HandleFunc("GET /{topic...}", func(w http.ResponseWriter, r *http.Request) {
+		topic := "/" + strings.TrimRight(r.PathValue("topic"), "/")
+		homeTemplate.Execute(w, "/.ws"+topic)
+	})
+
+	http.HandleFunc("GET /.ws/{topic...}", func(w http.ResponseWriter, r *http.Request) {
+		topic := "/" + strings.TrimRight(r.PathValue("topic"), "/")
+		serveWebsocket(topic, w, r)
+	})
+
+	log.Println("Server started at localhost:8888")
+
+	if err := http.ListenAndServe("localhost:8888", nil); err != nil {
+		log.Fatal(err)
+	}
+}
+
+var homeTemplate = template.Must(template.New("").Parse(`
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8">
+<script>  
+window.addEventListener("load", function(evt) {
+
+    var output = document.getElementById("output");
+    var input = document.getElementById("input");
+    var ws;
+
+    var print = function(message) {
+        var d = document.createElement("div");
+        d.textContent = message;
+        output.appendChild(d);
+        output.scroll(0, output.scrollHeight);
+    };
+
+    document.getElementById("open").onclick = function(evt) {
+        if (ws) {
+            return false;
+        }
+        ws = new WebSocket("{{.}}");
+        ws.onopen = function(evt) {
+            print("OPEN");
+        }
+        ws.onclose = function(evt) {
+            print("CLOSE");
+            ws = null;
+        }
+        ws.onmessage = function(evt) {
+            print("RESPONSE: " + evt.data);
+        }
+        ws.onerror = function(evt) {
+            print("ERROR: " + evt.data);
+        }
+        return false;
+    };
+
+    document.getElementById("send").onclick = function(evt) {
+        if (!ws) {
+            return false;
+        }
+        print("SEND: " + input.value);
+        ws.send(input.value);
+        return false;
+    };
+
+    document.getElementById("close").onclick = function(evt) {
+        if (!ws) {
+            return false;
+        }
+        ws.close();
+        return false;
+    };
+
+});
+</script>
+</head>
+<body>
+<table>
+<tr><td valign="top" width="50%">
+<p>Click "Open" to create a connection to the server, 
+"Send" to send a message to the server and "Close" to close the connection. 
+You can change the message and send multiple times.
+<p>
+<form>
+<button id="open">Open</button>
+<button id="close">Close</button>
+<p><input id="input" type="text" value="Hello world!">
+<button id="send">Send</button>
+</form>
+</td><td valign="top" width="50%">
+<div id="output" style="max-height: 70vh;overflow-y: scroll;"></div>
+</td></tr></table>
+</body>
+</html>
+`))
