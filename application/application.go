5ae2226b172a81ea24dc091ce3c78626c2631ab3
bliepp <12013202+bliepp@users.noreply.github.com>
2024-05-29 11:55:13 +0200

    Make websocket handler func a child of application struct

--- application/application.go
+++ application/application.go
@@ -1,13 +1,16 @@
 package application
 
 import (
+	"bytes"
 	"io"
 	"log"
 	"net/http"
 	"os"
 	"strings"
+	"time"
 
 	"github.com/fatih/color"
+	"github.com/gorilla/websocket"
 )
 
 var boldPrintf = color.New(color.Bold).PrintfFunc()
@@ -34,10 +37,80 @@ func New() *Application {
 	// actual websocket
 	a.router.HandleFunc("GET /.ws/{topic...}", func(w http.ResponseWriter, r *http.Request) {
 		topic := "/" + strings.TrimRight(r.PathValue("topic"), "/")
-		ServeWebsocketClient(topic, w, r)
+		a.handleWebsocket(topic, w, r)
 	})
 
 	return a
+}
+
+func (a *Application) handleWebsocket(topic string, w http.ResponseWriter, r *http.Request) {
+	b := NewBroadcast(topic)
+
+	conn, err := upgrader.Upgrade(w, r, nil)
+	if err != nil {
+		a.Logger.Printf("Error establishing websocket connection: %v", err)
+		return
+	}
+
+	client := b.NewClient(conn)
+	client.Register()
+
+	// getting messages from client and pushing them to the broadcast
+	go func() {
+		defer client.Unregister()
+
+		for {
+			_, msg, err := client.conn.ReadMessage()
+			if err != nil {
+				if websocket.IsUnexpectedCloseError(err, websocket.CloseGoingAway, websocket.CloseAbnormalClosure) {
+					a.Logger.Printf("Error: %v", err)
+				}
+				return
+			}
+
+			client.broadcast.messages <- bytes.TrimSpace(bytes.Replace(msg, []byte("\n"), []byte(" "), -1))
+		}
+	}()
+
+	// getting messages from the broadcast and pushing it to the current client
+	go func() {
+		defer client.Unregister()
+
+		ticker := time.NewTicker(time.Second)
+		defer ticker.Stop()
+
+		for {
+			select {
+			case msg, ok := <-client.send:
+				if !ok {
+					client.conn.WriteMessage(websocket.CloseMessage, []byte{})
+					return
+				}
+
+				w, err := client.conn.NextWriter(websocket.TextMessage)
+				if err != nil {
+					return
+				}
+				w.Write(msg)
+
+				// Add queued chat messages to the current websocket message.
+				n := len(client.send)
+				for i := 0; i < n; i++ {
+					w.Write([]byte("\n"))
+					w.Write(<-client.send)
+				}
+
+				if err := w.Close(); err != nil {
+					return
+				}
+			case <-ticker.C:
+				if err := client.conn.WriteMessage(websocket.PingMessage, nil); err != nil {
+					return
+				}
+			}
+		}
+	}()
+
 }
 
 func (a *Application) ListenAndServe(addr string) error {
--- application/client.go
+++ application/client.go
@@ -1,11 +1,6 @@
 package application
 
 import (
-	"bytes"
-	"log"
-	"net/http"
-	"time"
-
 	"github.com/gorilla/websocket"
 )
 
@@ -30,74 +25,4 @@ func (c *Client) Register() {
 func (c *Client) Unregister() {
 	c.broadcast.unregister <- c
 	c.conn.Close()
-}
-
-func ServeWebsocketClient(topic string, w http.ResponseWriter, r *http.Request) {
-	b := NewBroadcast(topic)
-
-	conn, err := upgrader.Upgrade(w, r, nil)
-	if err != nil {
-		log.Printf("Error establishing websocket connection: %v", err)
-		return
-	}
-
-	client := b.NewClient(conn)
-	client.Register()
-
-	// getting messages from client and pushing them to the broadcast
-	go func() {
-		defer client.Unregister()
-
-		for {
-			_, msg, err := client.conn.ReadMessage()
-			if err != nil {
-				if websocket.IsUnexpectedCloseError(err, websocket.CloseGoingAway, websocket.CloseAbnormalClosure) {
-					log.Printf("Error: %v", err)
-				}
-				return
-			}
-
-			client.broadcast.messages <- bytes.TrimSpace(bytes.Replace(msg, []byte("\n"), []byte(" "), -1))
-		}
-	}()
-
-	// getting messages from the broadcast and pushing it to the current client
-	go func() {
-		defer client.Unregister()
-
-		ticker := time.NewTicker(time.Second)
-		defer ticker.Stop()
-
-		for {
-			select {
-			case msg, ok := <-client.send:
-				if !ok {
-					client.conn.WriteMessage(websocket.CloseMessage, []byte{})
-					return
-				}
-
-				w, err := client.conn.NextWriter(websocket.TextMessage)
-				if err != nil {
-					return
-				}
-				w.Write(msg)
-
-				// Add queued chat messages to the current websocket message.
-				n := len(client.send)
-				for i := 0; i < n; i++ {
-					w.Write([]byte("\n"))
-					w.Write(<-client.send)
-				}
-
-				if err := w.Close(); err != nil {
-					return
-				}
-			case <-ticker.C:
-				if err := client.conn.WriteMessage(websocket.PingMessage, nil); err != nil {
-					return
-				}
-			}
-		}
-	}()
-
 }
